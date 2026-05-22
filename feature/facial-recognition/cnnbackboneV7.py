b334e7a833a2483bef69040e08235b32bd2f3ba4
S1487 <7696663@student.swin.edu.au>
2026-05-22 18:33:46 +1000

    added the cnn model used for it

---
+++ feature/facial-recognition/cnnbackboneV7.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, models
+
+
+
+model = models.Sequential()
+
+#data augmentation layers
+#model.add(layers.RandomFlip("horizontal", input_shape=(64, 64, 3)))
+#model.add(layers.RandomRotation(0.05))
+#model.add(layers.RandomZoom(0.1))
+#model.add(layers.RandomBrightness(0.2))
+
+model.add(layers.Conv2D(32, (5, 5), padding="same", input_shape=(64, 64, 3)))
+model.add(layers.BatchNormalization())
+model.add(layers.LeakyReLU(alpha=0.1))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3)))
+model.add(layers.BatchNormalization())
+model.add(layers.LeakyReLU(alpha=0.1))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3)))
+model.add(layers.BatchNormalization())
+model.add(layers.LeakyReLU(alpha=0.1))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(256, (3, 3)))
+model.add(layers.BatchNormalization())
+model.add(layers.LeakyReLU(alpha=0.1))
+model.add(layers.GlobalAveragePooling2D())
+model.add(layers.Dense(256))
+model.add(layers.BatchNormalization())
+model.add(layers.LeakyReLU(alpha=0.1))
+model.add(layers.Dropout(0.3))
+model.add(layers.Dense(256))
+model.add(layers.Lambda(lambda x: tf.math.l2_normalize(x, axis=1)))
+model.summary()
